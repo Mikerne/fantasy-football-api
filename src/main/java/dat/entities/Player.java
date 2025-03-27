b1052d81b4e7ab04493e168b760874714ab0203f
MickeJDengaard <cph-md405@cphbusiness.dk>
2025-03-27 10:41:07 +0100

    Alt vedrørende teams

--- src/main/java/dat/entities/Player.java
+++ src/main/java/dat/entities/Player.java
@@ -1,25 +1,30 @@
 package dat.entities;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
 @Entity
-@Table(name = "PLAYERS")
+@Table(name = "players")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Player {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String position;
 
+    // Diagrammet siger "string team", men relationen "can be part of" antyder, at en spiller hører til et team.
     @ManyToOne
-    @JoinColumn(name = "team_id")
+    @JoinColumn(name = "team_id", nullable = false)
     private Team team;
 
-    @Column(name = "performance_rating")
+    @Column(name = "performance_rating", nullable = false)
     private int performanceRating;
-
 }
