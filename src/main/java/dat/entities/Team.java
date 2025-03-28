7c52eb5902fef53723fd64bdb1fcbeeef4a09f59
MickeJDengaard <cph-md405@cphbusiness.dk>
2025-03-28 22:34:47 +0100

    fix af datatyper

--- src/main/java/dat/entities/Team.java
+++ src/main/java/dat/entities/Team.java
@@ -4,22 +4,18 @@ package dat.entities;
 import dat.security.entities.User;
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-
-import java.sql.Timestamp;
+import java.security.Timestamp;
 import java.util.List;
 
 @Entity
 @Table(name = "TEAMS")
-@Getter
-@Setter
+@Data
 public class Team {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private int id;
 
     private String name;
 
@@ -37,10 +33,6 @@ public class Team {
     @OneToMany(mappedBy = "team")
     private List<Player> players;
 
-    @OneToMany(mappedBy = "homeTeam")
-    private List<Match> homeMatches;
 
-    @OneToMany(mappedBy = "awayTeam")
-    private List<Match> awayMatches;
 
 }
