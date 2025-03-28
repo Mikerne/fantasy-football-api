2d21a0bf8a4f2de0d9b60fb4ecc354e3c3b636ff
MickeJDengaard <cph-md405@cphbusiness.dk>
2025-03-28 22:36:09 +0100

    fix af datatyper

--- src/main/java/dat/entities/Team.java
+++ src/main/java/dat/entities/Team.java
@@ -5,7 +5,7 @@ import dat.security.entities.User;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.security.Timestamp;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Entity
